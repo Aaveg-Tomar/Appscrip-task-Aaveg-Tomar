f1018380701aa69df014994f0a4413f831a1d3f6
Aaveg-Tomar <tomaraaveg@gmail.com>
2024-09-13 01:03:49 +0530

    changes

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
-import "./MainPage.css"
 import Banner from './Banner'
 
 const MainPage = () => {
